6c9f976d9f0ef3bab163666d37b4da56e298cb8f
Gaurav Padaliya <gauravpadaliya2000@gmail.com>
2022-10-20 07:04:45 +0530

    update apikee

--- src/App.js
+++ src/App.js
@@ -1,40 +1,46 @@
-import './App.css';
-import React, { useState , useEffect } from 'react';
+import "./App.css";
+import React, { useState, useEffect } from "react";
 // import ReactDOM from 'react-dom';
-import Nevbar from './component/nevBar';
-import axios from 'axios';
-import  NewsCont from './component/newsComp/NewsCont'
-import Footer from './component/Footer.js'
+import Nevbar from "./component/nevBar";
+import axios from "axios";
+import NewsCont from "./component/newsComp/NewsCont";
+import Footer from "./component/Footer.js";
 function App() {
   const [catagory, setCatagory] = useState("general");
   const [newsApi, setNewsApi] = useState([]);
   const [newsRes, setNewsRes] = useState();
   const [loadmore, setLoadmore] = useState(20);
-  const getApi = async ()=>{
-    try{
-      const res = await axios.get(`https://newsapi.org/v2/top-headlines?country=in&category=${catagory}&pageSize=${loadmore}&apiKey=95082c1e818d4654bc406f1a84aece42`);
+  const apiKey = "95082c1e818d4654bc406f1a84aece42";
+  const getApi = async () => {
+    try {
+      const res = await axios.get(
+        `https://newsapi.org/v2/top-headlines?country=in&category=${catagory}&pageSize=${loadmore}&apiKey=${apiKey}`
+      );
       // res.then(res => {
-        setNewsApi([...res.data.articles]);
-        setNewsRes(res.data.totalResults)
-        // console.log(newsApi);  
+      setNewsApi([...res.data.articles]);
+      setNewsRes(res.data.totalResults);
+      // console.log(newsApi);
       // })
-    }catch (err) {
+    } catch (err) {
       console.log(err);
     }
   };
 
   useEffect(() => {
     getApi();
-  }, [catagory,newsRes,loadmore])
-  
+  }, [catagory, newsRes, loadmore]);
+
   return (
     <div className="App">
-      
-        <Nevbar setCatagory = {setCatagory}/>
-        <NewsCont newsApi = {newsApi }loadmore = { loadmore} newsRes = {newsRes} setLoadmore = {setLoadmore} />
-        <Footer/>
+      <Nevbar setCatagory={setCatagory} />
+      <NewsCont
+        newsApi={newsApi}
+        loadmore={loadmore}
+        newsRes={newsRes}
+        setLoadmore={setLoadmore}
+      />
+      <Footer />
     </div>
-    
   );
 }
 
